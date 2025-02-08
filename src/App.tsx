96bcf766f3505e438fb253d62f1f73408395813c
Jemal-G <jgeda@madisoncollege.edu>
2025-02-08 10:42:18 -0600

    studied about marin padding...and colors

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,17 @@ function App() {
 
   return (
     <div>
-      <h1 className = 'text-2x1 font-bold underline'>
-      TCA Toss Coin
+      <h1 className = 'text-2x1 font-bold bg-base-300 p-4 ml-6 uppercase bg-sky-950 text-secondary'>
+        TCA Toss Coin
       </h1>
-      <button className='btn btn-secondary btn-soft'>play toss coin</button>
+      <div >
+        <button className='btn btn-secondary btn-soft btn-soft btn-xl p-4 ml-8 uppercase'>
+          play toss coin
+        </button>
+        <h2 className='mt-3 text-xl font-semi-bold'>
+            Leaderboard
+        </h2>
+      </div>
     </div>
   )
 }
