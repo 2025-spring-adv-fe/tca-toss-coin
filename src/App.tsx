4a4e03eb40febf92476403e24e8e5f077bd8cbf6
Jemal-G <jgeda@madisoncollege.edu>
2025-02-08 11:31:40 -0600

    studied more about card,positioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,102 @@
-
-
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-
   return (
     <div>
-      <h1 className = 'text-2x1 font-bold bg-base-300 p-4 ml-6 uppercase bg-sky-950 text-secondary'>
+      <h1 className="text-2x1 font-bold bg-base-300 p-4 ml-6 uppercase bg-sky-950 text-secondary">
         TCA Toss Coin
       </h1>
-      <div >
-        <button className='btn btn-secondary btn-soft btn-soft btn-xl p-4 ml-8 uppercase'>
-          play toss coin
+      <div>
+        <button className="btn btn-secondary btn-soft btn-soft btn-xl p-4 ml-8">
+          start to play
         </button>
-        <h2 className='mt-3 text-xl font-semi-bold'>
-            Leaderboard
+        <h2 className="mt-3 ml-6 text-xl font-semi-bold">Leaderboard
+
         </h2>
+
+        <div className="card-lg w-full bg-base-100 shadow-xl mt-4"
+        >
+          <div 
+          className="card-body"
+          >
+            <h2 
+            className="card-title"
+            >
+              Leaderboard 
+            </h2>
+            <p>Leaderboard coming soon...........</p>
+          </div>
+        </div>
+
+
+
+        <div className="card-lg w-full bg-base-100 shadow-xl mt-4"
+        >
+          <div 
+          className="card-body"
+          >
+            <h2 
+            className="card-title"
+            >
+              Leaderboard 
+            </h2>
+            <p>Leaderboard coming soon...........</p>
+          </div>
+        </div>
+
+
+
+        <div className="card-lg w-full bg-base-100 shadow-xl mt-4"
+        >
+          <div 
+          className="card-body"
+          >
+            <h2 
+            className="card-title"
+            >
+              Leaderboard 
+            </h2>
+            <p>Leaderboard coming soon...........</p>
+          </div>
+        </div>
+
+
+
+
+        <div className="card-lg w-full bg-base-100 shadow-xl mt-4"
+        >
+          <div 
+          className="card-body"
+          >
+            <h2 
+            className="card-title"
+            >
+              Leaderboard 
+            </h2>
+            <p>Leaderboard coming soon...........</p>
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
