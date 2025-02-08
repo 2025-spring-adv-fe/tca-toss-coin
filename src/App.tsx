a986db2f4d06146c19ae7d5ee2e44441ddbeab09
Jemal-G <jgeda@madisoncollege.edu>
2025-02-08 11:50:39 -0600

    play withchat buble

--- src/App.tsx
+++ src/App.tsx
@@ -10,90 +10,74 @@ function App() {
         <button className="btn btn-secondary btn-soft btn-soft btn-xl p-4 ml-8">
           start to play
         </button>
-        <h2 className="mt-3 ml-6 text-xl font-semi-bold">Leaderboard
+        <h2 className="mt-3 ml-6 text-xl font-semi-bold">Leaderboard</h2>
 
-        </h2>
-
-        <div className="card-lg w-full bg-base-100 shadow-xl mt-4"
-        >
-          <div 
-          className="card-body"
-          >
-            <h2 
-            className="card-title"
-            >
-              Leaderboard 
-            </h2>
+        <div className="card-lg w-full bg-base-100 shadow-xl mt-4">
+          <div className="card-body">
+            <h2 className="card-title">Leaderboard</h2>
             <p>Leaderboard coming soon...........</p>
           </div>
         </div>
 
-
-
-        <div className="card-lg w-full bg-base-100 shadow-xl mt-4"
-        >
-          <div 
-          className="card-body"
-          >
-            <h2 
-            className="card-title"
-            >
-              Leaderboard 
-            </h2>
+        <div className="card-lg w-full bg-base-100 shadow-xl mt-4">
+          <div className="card-body">
+            <h2 className="card-title">Leaderboard</h2>
             <p>Leaderboard coming soon...........</p>
           </div>
         </div>
 
-
-
-        <div className="card-lg w-full bg-base-100 shadow-xl mt-4"
-        >
-          <div 
-          className="card-body"
-          >
-            <h2 
-            className="card-title"
-            >
-              Leaderboard 
-            </h2>
+        <div className="card-lg w-full bg-base-100 shadow-xl mt-4">
+          <div className="card-body">
+            <h2 className="card-title">Leaderboard</h2>
             <p>Leaderboard coming soon...........</p>
           </div>
         </div>
 
+        <div className="card-lg w-full bg-base-100 shadow-xl mt-4">
+          <div className="card-body">
+            <h2 className="card-title">Leaderboard</h2>
 
 
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-primary">
+                What kind of nonsense is this
+              </div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-secondary">
+                Put me on the Council and not make me a Master!??
+              </div>
+            </div>
+            <div className="chat chat-start">
+              <div className="chat-bubble chat-bubble-accent">
+                That's never been done in the history of the Jedi. It's
+                insulting!
+              </div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-info">
+                Calm down, Anakin.
+              </div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-success">
+                You have been given a great honor.
+              </div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-warning">
+                To be on the Council at your age.
+              </div>
+            </div>
+            <div className="chat chat-end">
+              <div className="chat-bubble chat-bubble-error">
+                It's never happened before.
+              </div>
+            </div>
 
-        <div className="card-lg w-full bg-base-100 shadow-xl mt-4"
-        >
-          <div 
-          className="card-body"
-          >
-            <h2 
-            className="card-title"
-            >
-              Leaderboard 
-            </h2>
-            <p>Leaderboard coming soon...........</p>
+            
           </div>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       </div>
     </div>
   );
