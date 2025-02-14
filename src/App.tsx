d73b87d26943846c5ac75666c5b302dde8c5c2be
Jemal-G <jgeda@madisoncollege.edu>
2025-02-14 11:24:53 -0600

    review all changes and added some daisyui and div

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-
-
-import './App.css'
+import "./App.css";
 
 function App() {
- 
-
   return (
     <div>
-      <h1 className = 'text-2x1 font-bold underline'>
-      TCA Toss Coin
-      </h1>
-      <button className='btn btn-secondary btn-soft'>play toss coin</button>
+      <h1 className="text-2xl font-bold bg-base-300 p-4">TCA Toss Coin</h1>
+      <div className="p-4"
+      >
+        <button className="btn btn-secondary btn-outline btn-soft btn-xl p-6">
+          Play Toss Coin
+        </button>
+        <h2 className="mt-3 font-semi-bold">Leaderboard</h2>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
