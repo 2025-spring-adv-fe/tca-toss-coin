943b011b59d25c241252b7cba5e3e8da5e15eeb6
Jemal-G <jgeda@madisoncollege.edu>
2025-02-20 13:03:46 -0600

    can navigate from home to setup through button

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,21 @@
+import { useNavigate } from "react-router";
+
 export const Home = () => {
+    //use react hook or button navigation
+    const nav = useNavigate();
   return (
     <>
-      <h3 className="text-2xl font-bold">Home</h3>
-      <button className="btn btn-active btn-secondary btn-lg mt-4">
+      <h3 
+      className="text-2xl font-bold"
+      >
+        Home
+        </h3>
+      <button 
+      className="btn btn-active btn-secondary btn-lg mt-4"
+      onClick={
+         ()=>nav("/setup") 
+      }
+      >
         toss coin
       </button>
     </>
