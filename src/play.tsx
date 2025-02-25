620ee36114eeca1eaf365949b99bf39d2be19d06
Jemal-G <jgeda@madisoncollege.edu>
2025-02-25 13:10:27 -0600

    use is groovy!!!!

--- src/play.tsx
+++ src/play.tsx
@@ -1,7 +1,12 @@
 import { useNavigate } from "react-router";
+import { useState } from "react";
 
 export const Play = () => {
   const nav = useNavigate();
+
+  const [turnNumber, setTurnNumber] = useState(6);
+  
+  //let turnNumber = 6;
   return (
     <>
       <h3 
@@ -9,6 +14,21 @@ export const Play = () => {
       >
         play
         </h3>
+       <h4 
+       className="text-lg font-semibold"
+       >
+        Turn #{turnNumber}
+        <button 
+        className="btn btn-active btn-secondary btn-lg mt-4 "
+        onClick={() => {
+         setTurnNumber(turnNumber + 1); 
+          console.log("Turn Number:", turnNumber); 
+         }}
+        >
+          +
+        </button>
+       </h4>
+    
       <button 
       className="btn btn-active btn-secondary btn-lg mt-4"
       onClick={
